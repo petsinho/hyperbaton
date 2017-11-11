1c40cd1ed6cdc514b7c70860986f538eede7f7b9
Panagiotis Petsas <panagiotis.petsas@gmail.com>
2017-11-11 14:35:44 +0100

    fiddling with appspec.yml, added lintrc

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-var port = process.env.PORT || 3000,
-    http = require('http'),
-    fs = require('fs');
+const port = process.env.PORT || 3000;
+const http = require('http');
+const fs = require('fs');
 
 
-http.createServer(function (req, res) {
-  if (req.url.indexOf('/img') != -1) {
+http.createServer((req, res) => {
+  if (req.url.indexOf('/img') !== -1) {
     var filePath = req.url.split('/img')[1];
-    fs.readFile(__dirname + '/public/img' + filePath, function (err, data) {
+    fs.readFile(`${__dirname}/public/img${filePath}`, (err, data) => {
       if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+      res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
       res.write(data);
       res.end();
     });
-  } else if (req.url.indexOf('/js') != -1) {
+  } else if (req.url.indexOf('/js') !== -1) {
     var filePath = req.url.split('/js')[1];
-    fs.readFile(__dirname + '/src/js' + filePath, function (err, data) {
+    fs.readFile(`${__dirname}/src/js${filePath}`, (err, data) => {
       if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.writeHead(200, { 'Content-Type': 'text/javascript' });
       res.write(data);
       res.end();
     });
-  } else if(req.url.indexOf('/css') != -1) {
+  } else if (req.url.indexOf('/css') !== -1) {
     var filePath = req.url.split('/css')[1];
-    fs.readFile(__dirname + '/public/css' + filePath, function (err, data) {
+    fs.readFile(`${__dirname}/public/css${filePath}`, (err, data) => {
       if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.writeHead(200, { 'Content-Type': 'text/css' });
       res.write(data);
       res.end();
     });
   } else {
-    fs.readFile(__dirname + '/public/index.html', function (err, data) {
+    fs.readFile(`${__dirname}/public/index.html`, (err, data) => {
       if (err) console.log(err);
-      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write(data);
       res.end();
     });
